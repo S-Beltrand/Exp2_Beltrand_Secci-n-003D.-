4f84b99633a2c92dae83066594947d3bd288a648
S-Beltrand <80703838+S-Beltrand@users.noreply.github.com>
2021-05-17 18:17:10 -0400

    se agregaron filas a los archivos ya creados

--- Proyecto web/js/funciones.js
+++ Proyecto web/js/funciones.js
@@ -5,3 +5,40 @@ $('.checkbox').click(function(){
         $('.theme').attr('href', 'css/light.css');
     }
 });
+$(function(){
+    $("#formuralito").validate({
+      rules: {
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    name: "required",
+                    ape: "required",
+                    fecha: "required",
+                    fono:"required"
+                    
+                }, 
+            messages: {
+                email: {
+                    required: 'Ingresa tu correo electrónico',
+                    email: 'Formato de correo no válido'
+                },
+                name: {
+                    required: 'Ingresa tu Nombre',
+                    minlength: 'Caracteres insuficientes'
+                },
+                ape: {
+                    required: 'Ingresa tu Apellido',
+                    minlength: 'Caracteres insuficientes'
+                },
+                fono:{
+                    required: 'Ingrese un número de celular',
+                    minlength: 'Cantidad de digitos insuficiente'
+                },
+                fecha:{
+                    required: 'Seleccione una fecha válida',
+                    max: 'Fecha no valida'
+                }
+            }
+        }); 
+    }); 
